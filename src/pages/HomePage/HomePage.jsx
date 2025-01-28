89a48de45dc634f33601fdae43feb6927ac18d92
Antonio Gisondi <anto.gisondi@tiscali.it>
2025-01-28 18:17:33 +0100

    create api endpoint

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../index.css'
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Benvenuto nella Homepage</h1>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+function LoginPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LoginPage
